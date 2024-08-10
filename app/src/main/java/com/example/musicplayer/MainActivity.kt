e3e5b7d9d85ac030b79f4427f9b59107ccf3391d
ParthPrajapati <113522510+ParthPrajapati73@users.noreply.github.com>
2024-08-10 12:53:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/musicplayer/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.musicplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+       * User Guide for my app first follow this step  .
+       * start app and it ask for permission .
+       * allow permission for read and write.
+       * select song for your device and then play it
+       * working on further futures for add in my app
+       * this code on my github ParthPrajapati73
+       */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
